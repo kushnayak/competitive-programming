9988c4178d3712e4389b43e2dd52c12610bd8074
kush <kushnayak123@gmail.com>
2020-11-24 20:48:43 -0800

    updated java problems

--- src/USACO/Bronze/u2020/USOpen/CowTractTracing/CowTract.java
+++ src/USACO/Bronze/u2020/USOpen/CowTractTracing/CowTract.java
@@ -5,38 +5,75 @@ import java.util.*;
 public class CowTract {
 
 
-    public static void main(String[] args) throws IOException {
-        InputReader reader = new InputReader(".in");
-        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(".out")));
-        int N = reader.nextInt();
-        int T = reader.nextInt();
-        final int MAX = 251;
-        boolean[] infected = new boolean[N+1];
-        int[] x = new int[MAX], y = new int[MAX];
-        String inf = reader.next();
-        for (int i=1; i<=N; i++) {
-            if (inf.charAt(i)==1)
-                infected[i] = false;
-        }
-        for (int i=0;i<T;i++) {
-            int t = reader.nextInt();
-            x[t] = reader.nextInt();
-            y[t] = reader.nextInt();
-        }
-        for (int i=1;i<=N;i++) {
-            int minK = Integer.MAX_VALUE;
-            int maxK = 0;
-            boolean[] curInfected = new boolean[N];
-            curInfected[i] = true;
-            for (int j=0; j<MAX; j++) {
-                if (!(x[j]==0)) {
-                    if (curInfected[x[j]]||curInfected[y[j]]) {
-                        curInfected[x[j]] = true;
-                        curInfected[y[j]] = true;
-                    }
-                }
+    static int N, T;
+    static final int MAX_T = 251;
+    static int[][] interactions;
+    static boolean[] infected;
+
+
+    static boolean consistentWithData(int patientZero, int k) {
+        int[] interactionCounter = new int[N];
+        boolean[] currentInfected = new boolean[N];
+        currentInfected[patientZero-1] = true;
+        for (int i=0; i<MAX_T; i++) {
+            if (interactions[i][0]==0)
+                continue;
+            int x = interactions[i][0]-1;
+            int y = interactions[i][0]-1;
+            if ( (currentInfected[x] || currentInfected[y])
+                    && interactionCounter[x] <= k && interactionCounter[y] <= k) {
+                currentInfected[x] = true;
+                currentInfected[y] = true;
+                interactionCounter[x]++;
+                interactionCounter[y]++;
             }
         }
+        return Arrays.equals(infected, currentInfected);
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputReader reader = new InputReader("tracing.in");
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("tracing.out")));
+        N = reader.nextInt();
+        T = reader.nextInt();
+        String binary = reader.next();
+        infected = new boolean[N];
+        interactions = new int[MAX_T][2];
+        for (int i=0; i<N; i++)
+            if (binary.charAt(i) == '1')
+                infected[i] = true;
+
+        for (int i=0; i<T; i++) {
+            int time = reader.nextInt();
+            interactions[time][0] = reader.nextInt();
+            interactions[time][1] = reader.nextInt();
+        }
+        boolean infinity = false;
+        int total = 0;
+        int maxK = 0;
+        int minK = Integer.MAX_VALUE;
+        for (int i=1; i<=N; i++) {
+            boolean consistentOnce = false;
+            for (int j=0; j<MAX_T; j++) {
+                if(consistentWithData(i, j)) {
+                    consistentOnce = true;
+                    if (j==T)
+                        infinity = true;
+                    maxK = Math.max(maxK, j);
+                    minK = Math.min(minK, j);
+                }
+            }
+
+            if (consistentOnce)
+                total++;
+
+        }
+        writer.print(total + " ");
+        writer.print(minK + " ");
+        if (infinity)
+            writer.println("Infinity");
+        else
+            writer.println(maxK);
 
 
         writer.close();
--- src/scratch.java
+++ src/scratch.java
@@ -6,8 +6,28 @@ import java.util.StringTokenizer;
 public class scratch {
 
     public static void main(String[] args) throws IOException {
-        boolean[] arr = {true, true, false};
-        boolean[] arr1 = {true, true, false};
-        System.out.println(Arrays.equals(arr, arr1));
+        int[] arr1 = {-1, 0, 3, 3, 3, 10, 12};
+        int[] arr2 = {-1, 0, 3, 12};
+        System.out.println(linearIn(arr1, arr2));
+    }
+    public static boolean linearIn(int[] outer, int[] inner) {
+        int outerLen = outer.length;
+        int innerLen = inner.length;
+        int pointer = 0;
+        for (int i=0; i<innerLen; i++) {
+            int find = inner[i];
+            boolean found = false;
+            System.out.println(pointer);
+            while (pointer < outerLen && outer[pointer] <= find) {
+                if (outer[pointer] == find) {
+                    found = true;
+                    break;
+                }
+                pointer++;
+            }
+            if (!found)
+                return false;
+        }
+        return true;
     }
 }
